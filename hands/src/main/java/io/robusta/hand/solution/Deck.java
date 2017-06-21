9d46ad0d43ea0a4214e8c4fdfeb79feed96e35ef
Quntn <hinet.q@gmail.com>
2017-06-21 13:32:06 +0200

    working on Hand Test

--- hands/src/main/java/io/robusta/hand/solution/Deck.java
+++ hands/src/main/java/io/robusta/hand/solution/Deck.java
@@ -6,40 +6,52 @@ import java.util.Random;
 import java.util.TreeSet;
 
 import io.robusta.hand.Card;
-import io.robusta.hand.CardColor;
 import io.robusta.hand.interfaces.IDeck;
 
-public class Deck extends LinkedList<Card> implements IDeck{
+public class Deck extends LinkedList<Card> implements IDeck {
 
-	
 	private static final long serialVersionUID = -4686285366508321800L;
-	
+
 	public Deck() {
 
 	}
-	
+
 	@Override
 	public Card pick() {
-		// shuffle;
-		// remove card from deck and returns it
-		return null;
+
+		Collections.shuffle(this);
+		return this.pollFirst();
+
 	}
-
-
-	
 
 	@Override
 	public TreeSet<Card> pick(int number) {
 		// reuse pick()
-		return null;
+
+		int i = 0;
+		TreeSet<Card> cards = new TreeSet<>();
+
+		while (i < number) {
+
+			cards.add(this.pick());
+			i++;
+		}
+		return cards;
+
 	}
+
+
+
 
 	@Override
 	public Hand giveHand() {
 		// A hand is a **5** card TreeSet
-		return null;
+		TreeSet<Card> cards = pick(5);   //récupère les cartes
+		
+		Hand hand = new Hand();  		//crée ma main
+		hand.addAll(cards);				//ajoute les cartes dans la main
+		
+		return hand;
 	}
-	
-	
-	
+
 }
